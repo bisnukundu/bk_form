b0132f823293fc6f1fb018f466a2701dc12c9a41
bisnukundu <bisnukundu@gmail.com>
2026-01-31 01:18:36 +0600

    vendor folder added

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'bisnukundu/bk-form',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd5acf67fa8951a4e3367e5f3e6a737c3c1dbd54d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'bisnukundu/bk-form' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd5acf67fa8951a4e3367e5f3e6a737c3c1dbd54d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
